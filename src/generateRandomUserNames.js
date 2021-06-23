58488b35ddd80ae9a1ec77b9d3b9eb7642dd96b6
Evan Peterson <evan.peterson216@gmail.com>
2021-06-23 10:14:39 -0400

    whitespace

--- src/generateRandomUserNames.js
+++ src/generateRandomUserNames.js
@@ -1,17 +1,18 @@
 const adjectives = require('./adjectives');
 const animals = require('./animals');
 
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomNumber = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
 
 const getRandomItemFromArray = (array) => getRandomNumber(0, array.length - 1);
 
 const generateRandomUsername = () => {
-	return adjectives[getRandomItemFromArray(adjectives)]
-	  + "-"
-	  + animals[getRandomItemFromArray(animals)]
-	  + getRandomNumber(0, 999)
+  return adjectives[getRandomItemFromArray(adjectives)]
+	+ "-"
+	+ animals[getRandomItemFromArray(animals)]
+	+ getRandomNumber(0, 999)
 }
 
 module.exports = {
   generateRandomUsername,
+  getRandomNumber,
 }
