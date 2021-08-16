2548ee00aa2db6bbffc151f7e8eee9e1cf30ea40
Evan Peterson <evan.peterson216@gmail.com>
2021-08-16 12:41:29 -0400

    add option to remove random numbers

--- src/__tests__/generateRandomUserNames.test.js
+++ src/__tests__/generateRandomUserNames.test.js
@@ -1,8 +1,37 @@
 const { generateRandomUsername } = require('../generateRandomUserNames');
 
 describe(('username generation tests'), () => {
-  it('should generate a random username', () => {
-    const randomUsername = generateRandomUsername();
-    expect(randomUsername).toEqual(expect.any(String))
+  it('should generate a random username with no options', () => {
+    const username = generateRandomUsername();
+    expect(username).toEqual(expect.any(String))
+  })
+
+  it('should generate username if options object has no keys', () => {
+    const username = generateRandomUsername({});
+
+    expect(username).toEqual(expect.any(String))
+  })
+
+  it('should remove the hyphen if options.useHyphen option is false', () => {
+    const usernameWithoutHyphen = generateRandomUsername({ useHyphen: false })
+
+    expect(usernameWithoutHyphen).toEqual(expect.any(String))
+    expect(usernameWithoutHyphen.includes('-')).toBe(false)
+  })
+
+  it('should remove numbers if options.useRandomNumber is false', () => {
+    const usernameWithoutNumber = generateRandomUsername({ useRandomNumber: false })
+    const matchNumbersRegex = /[0-9]/g
+    
+    expect(usernameWithoutNumber).toEqual(expect.any(String))
+    expect(usernameWithoutNumber.match(matchNumbersRegex)).toBe(null)
+  })
+
+  it('should throw error if non-object is passed as options', () => {
+    expect(() => generateRandomUsername('this is wrong! throw error')).toThrowError('options must be an object')
+  })
+
+  it('should throw error if useHyphen option is not a boolean', () => {
+    expect(() => generateRandomUsername({ useHyphen: 'huehuehue' })).toThrowError(`options.useHyphen must be a boolean, got string instead.`)
   })
 });
--- src/generateRandomUserNames.js
+++ src/generateRandomUserNames.js
@@ -5,11 +5,47 @@ const getRandomNumber = (min, max) => (Math.floor(Math.random() * (max - min + 1
 
 const getRandomItemFromArray = (array) => getRandomNumber(0, array.length - 1);
 
-const generateRandomUsername = () => {
-  return adjectives[getRandomItemFromArray(adjectives)]
-	+ "-"
-	+ animals[getRandomItemFromArray(animals)]
-	+ getRandomNumber(0, 999)
+const defaultOptions = {
+  useHyphen: true,
+  useRandomNumber: true
+}
+
+const checkOptions = (options) => {
+  if (typeof options !== 'object') {
+    throw new Error('options must be an object')
+  }
+
+  const propertyExists = (property) => (typeof options[property] !== 'undefined')
+
+  if (propertyExists('useHyphen') && (typeof options.useHyphen !== 'boolean')) {
+    throw new Error(`options.useHyphen must be a boolean, got ${typeof options.useHyphen} instead.`)
+  }
+
+  if (propertyExists('useRandomNumber') && (typeof options.useRandomNumber !== 'boolean')) {
+    throw new Error(`options.useRandomNumber must be a boolean, got ${typeof options.useRandomNumber} instead.`)
+  }
+}
+
+const generateRandomUsername = (options = defaultOptions) => {
+
+  checkOptions(options) // Input Validation
+  
+  // Construct username
+  const randomAdjective = adjectives[getRandomItemFromArray(adjectives)]
+  const randomAnimal = animals[getRandomItemFromArray(animals)]
+  const randomNumber = getRandomNumber(0, 999)
+  let username = `${randomAdjective}-${randomAnimal}${randomNumber}` 
+
+  if (!options.useHyphen){
+    username = username.replace('-', '')
+  }
+
+  if (!options.useRandomNumber) {
+    const matchNumbersRegex = /[0-9]/g
+    username = username.replace(matchNumbersRegex, '')
+  }
+
+  return username;
 }
 
 module.exports = {
